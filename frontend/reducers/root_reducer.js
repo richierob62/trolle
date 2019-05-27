2e700540889dbf350e4407fc891e8d61bf216a8e
richierob62 <richierob62@gmail.com>
2019-05-27 12:31:15 -0400

    Immer test

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -6,10 +6,11 @@ import session from './session_reducer'
 import errors from './errors_reducer'
 
 const rootReducer = combineReducers({
-  entities,
-  session,
-  ui,
-  errors
+  // entities,
+  // session,
+  // ui,
+  // errors
+  foo: (state = {}, action) => state
 })
 
 export default rootReducer
