bed310882e8e3739ecfd0080b742d82398470074
richierob62 <richierob62@gmail.com>
2019-05-30 18:10:14 -0400

    Drag and drop styling complete

--- frontend/components/card.jsx
+++ frontend/components/card.jsx
@@ -4,10 +4,22 @@ import { Draggable } from 'react-beautiful-dnd'
 export default ({ card, index }) => (
   <Draggable draggableId={card.id} index={index}>
     {(provided, snapshot) => {
+      if (snapshot.isDragging) {
+        return (
+          <div
+            className="dragged-card"
+            data-title={card.title}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+          >
+            {card.title}
+          </div>
+        )
+      }
       return (
         <div
-          id={`card-${card.id}`}
-          className={`card ${snapshot.isDragging ? 'is-dragging' : ''}`}
+          className="card"
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
