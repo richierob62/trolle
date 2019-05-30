507f426a1bb783c50877b149dad6e739902183d5
richierob62 <richierob62@gmail.com>
2019-05-29 22:01:29 -0400

    Fixed active link in menu on home button click

--- frontend/components/home_menu.jsx
+++ frontend/components/home_menu.jsx
@@ -6,7 +6,7 @@ import { setSelectedHomeMenuItem } from '../actions/ui_actions'
 
 const mstp = state => ({
   teams: Object.values(state.entities.teams),
-  selectedItem: state.ui.nav.home_menu,
+  selectedItem: state.ui.nav.home_menu || 'boards'
 })
 
 const mdtp = dispatch => ({
@@ -99,9 +99,6 @@ class HomeMenu extends React.Component {
               selectorFunc={this.handleItemSelect('team_' + team.id)}
             />
           ))}
-          <li>
-            <span>i</span>Create a team
-          </li>
         </ul>
       </nav>
     )
